6dd292c5a81d5d2fbb2ae6f1ce6af63ca9aed11c
ali4728 <erkan.ali.yilmaz@gmail.com>
2026-04-12 18:07:56 -0700

    assembly name update

--- ScintillaNET.Demo/Properties/AssemblyInfo.cs
+++ ScintillaNET.Demo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScintillaNET.Demo")]
+[assembly: AssemblyTitle("AnyText")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("None")]
-[assembly: AssemblyProduct("ScintillaNET.Demo")]
+[assembly: AssemblyProduct("AnyText")]
 [assembly: AssemblyCopyright("Copyright © None 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
